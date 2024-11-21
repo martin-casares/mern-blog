78ae131d97569131a9c858be58374f3488124788
martin <casaresrm@gmail.com>
2024-11-21 17:05:04 -0300

    Header con tailwindcss

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
 export default function Header() {
   const navigate = useNavigate();
   const { userInfo, setUserInfo } = useContext(UserContext);
+  const [isActive, setIsActive] = useState(false);
 
   function logout() {
     fetch("http://localhost:4000/auth/logout", {
@@ -16,28 +17,65 @@ export default function Header() {
     });
   }
 
+  useEffect(() => {
+    const scrollMe = () => {
+      window.scrollY > 50 ? setIsActive(true) : setIsActive(false);
+    };
+    window.addEventListener("scroll", scrollMe);
+  }, []);
+
   //console.log(userInfo);
   return (
-    <header>
-      <Link to="/" className="logo">
-        Blog
-      </Link>
-      <nav>
-        {userInfo ? ( // Si hay userInfo, muestra las opciones de usuario autenticado
-          <>
-            <Link to="/create">Create new post</Link>
-            <a href="#" onClick={logout}>
-              Logout
-            </a>
-          </>
-        ) : (
-          // Si no hay userInfo, mostramos las opciones de login/registro
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
-        )}
-      </nav>
+    <header
+      className={`border-b border-black sticky top-0 z-50 
+     ${isActive ? "bg-white" : "bg-banner"} transition-all duration-500 `}
+    >
+      <div className="size h-[70px] flex items-center justify-between">
+        <Link to="/" className="h-[1.5rem] font-bold">
+          Blog
+        </Link>
+        <nav className="flex  items-center gap-5">
+          <div className="flex gap-5">
+            <Link>About</Link>
+            <Link>dos</Link>
+            <Link>Contact</Link>
+          </div>
+          {userInfo ? ( // Si hay userInfo, muestra las opciones de usuario autenticado
+            <div>
+              <Link to="/create" className="m-5 mr-2">
+                Create new post
+              </Link>
+              <a
+                href="#"
+                onClick={logout}
+                className={`bg-black text-white rounded-full px-3 p-2 text-sm font-medium ${
+                  isActive ? "bg-green-900" : "bg-black"
+                }`}
+              >
+                Logout
+              </a>
+            </div>
+          ) : (
+            // Si no hay userInfo, mostramos las opciones de login/registro
+            <>
+              <div className="relative">
+                <Link
+                  to="/login"
+                  className="ml-5 text-sm sm:flex items-center gap-5"
+                >
+                  Login
+                </Link>
+              </div>
+              <Link
+                to="/register"
+                className="bg-black text-white rounded-full px-3 p-2 text-sm font-medium"
+              >
+                Register
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
     </header>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: "rgba(0,0,0,0.8)",
+        banner: "rgb(255, 192, 23)",
+      },
+      fontFamily: {
+        title: `gt-super, Georgia, Cambria, Times New Roman, Times, serf;`,
+        texts: `sohne, Helvetica Neue, Helvetica, Arial, sans-serif`,
+      },
+      gridTemplateColumns: {
+        card: "repeat(auto-fit, minmax(280px, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
-
+};
